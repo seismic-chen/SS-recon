6d84b6aee06a0a4a82c602df229ad30f7c5f1700
Yunfeng Chen <yunfeng1@ualberta.ca>
2022-05-06 22:41:25 +0800

    Delete FMI directory

--- FMI/@MatSeismogram/enable_iterator.m
+++
@@ -1,9 +0,0 @@
-function mySeis = enable_iterator(mySeis);
-
-% ENABLE_ITERATOR Enable iterator 
-%
-% Note: The iterator is disabled initially when you new a MatSeismogram object. 
-%
-% See also disable_iterator, init_iterator
-
-mySeis.iterator.enabled = 1;
